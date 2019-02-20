0a1d49577cef974158b2e40f28c437d0fd70f48e
Tim <github@cybertim.net>
2019-02-20 21:25:52 +0100

    working on osx

---
+++ jpack.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+export JAVA_HOME=$(/usr/libexec/java_home)
+/Users/tim/Downloads/jdk.packager-osx/jpackager create-image --verbose -i ./shade -o ./target -n "Toebify" -j "toebify.jar" -c "com.dskwrk.java.Launcher" --add-modules java.base,java.datatransfer,java.desktop,java.scripting,java.xml,jdk.jsobject,jdk.unsupported,jdk.unsupported.desktop,jdk.xml.dom,java.naming,java.sql,jdk.charsets
